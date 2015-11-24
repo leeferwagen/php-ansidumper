9e58b37feaf907689f6545337a45a98d6f18b207
Jan Oetjen <jo@onscreen.net>
2015-11-24 13:46:06 +0100

    composerified

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ use AnsiDumper\FD;
 require('vendor/autoload.php');
 
 #define('FD_FILE', '/tmp/fd.dump');
-$_SERVER['FD_FILE'] = '/tmp/fd.dump';
+$_SERVER['FD_FILE'] = 'fd.dump';
 
 FD::time('foo');
 usleep(rand(1000000, 2000000));
